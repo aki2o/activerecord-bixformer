57e7aeb77929a41e37245842469248d8875bc900
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2016-09-09 19:08:08 +0900

    fix bug

--- lib/activerecord-bixformer/shared/value_presence_validatable.rb
+++ lib/activerecord-bixformer/shared/value_presence_validatable.rb
@@ -3,9 +3,9 @@ module ActiveRecord::Bixformer::ValuePresenceValidatable
     # 空でない要素であるか or 空でない要素を含んでいるかどうか
     case value
     when ::Hash
-      value.values.any? { |v| valid_value?(v) }
+      value.values.any? { |v| presence_value?(v) }
     when ::Array
-      value.any? { |v| valid_value?(v) }
+      value.any? { |v| presence_value?(v) }
     when ::String
       ! value.blank?
     when ::TrueClass, ::FalseClass
