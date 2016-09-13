2445b21a8089864c845987fde7b7abeba28dfcda
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2016-09-13 15:46:40 +0900

    use autoload

--- lib/activerecord-bixformer.rb
+++ lib/activerecord-bixformer.rb
@@ -1,8 +1,44 @@
 require 'activerecord-bixformer/version'
-Dir["#{File.dirname(__FILE__)}/activerecord-bixformer/**/base.rb"].each { |f| require f }
-Dir["#{File.dirname(__FILE__)}/activerecord-bixformer/**/*.rb"].each { |f| require f }
 
 module ActiveRecord
   module Bixformer
+    module Attribute
+      autoload :Base,       'activerecord-bixformer/attribute/base'
+      autoload :Boolean,    'activerecord-bixformer/attribute/boolean'
+      autoload :Booletania, 'activerecord-bixformer/attribute/booletania'
+      autoload :Date,       'activerecord-bixformer/attribute/date'
+      autoload :Enumerize,  'activerecord-bixformer/attribute/enumerize'
+      autoload :Override,   'activerecord-bixformer/attribute/override'
+      autoload :Time,       'activerecord-bixformer/attribute/time'
+    end
+
+    module Generator
+      autoload :ActiveRecord, 'activerecord-bixformer/generator/active_record'
+      autoload :Base,         'activerecord-bixformer/generator/base'
+      autoload :CsvRow,       'activerecord-bixformer/generator/csv_row'
+    end
+
+    module Model
+      autoload :Base, 'activerecord-bixformer/model/base'
+
+      module Csv
+        autoload :Base,    'activerecord-bixformer/model/csv/base'
+        autoload :Indexed, 'activerecord-bixformer/model/csv/indexed'
+      end
+    end
+
+    module Modeler
+      autoload :Base, 'activerecord-bixformer/modeler/base'
+      autoload :Csv, 'activerecord-bixformer/modeler/csv'
+    end
+
+    module Runner
+      autoload :Base, 'activerecord-bixformer/runner/base'
+      autoload :Csv, 'activerecord-bixformer/runner/csv'
+    end
+
+    module Translator
+      autoload :I18n, 'activerecord-bixformer/translator/i18n'
+    end
   end
 end
